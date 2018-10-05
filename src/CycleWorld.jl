b9b7bbd458667639becae49e0da94a34d5ebaf42
Matt <mkschleg@umail.iu.edu>
2018-10-05 13:39:30 -0600

    Started for the discovery project

---
+++ src/CycleWorld.jl
@@ -0,0 +1,15 @@
+module CycleWorld
+
+mutable struct Environment
+    state
+
+end
+
+function get_observation(env::Environment)
+end
+
+
+
+
+
+end
---
+++ src/GVFN.jl
@@ -0,0 +1,9 @@
+module GVFN
+
+# greet() = print("Hello World!")
+export LinearRL
+
+include("LinearRL.jl")
+
+
+end # module
