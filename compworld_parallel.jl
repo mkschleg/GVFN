9c9fc5da994fe5208d3af44bbd3fa60f78ad4f0f
Matt <mkschleg@umail.iu.edu>
2019-03-27 10:43:42 -0600

    Updated compass world parallel experiment for rtd, tdlambda, and RMSProp.

--- compworld_parallel.jl
+++ compworld_parallel.jl
@@ -6,32 +6,61 @@ Pkg.activate(".")
 include("parallel_experiment.jl")
 
 println("Hello Wolrd...")
-function make_arguments(args::Dict{String, String})
+
+#------ Learning Updates -------#
+
+const learning_update = "RTD"
+const truncations = [1, 10, 24]
+
+const learning_update = "TDLambda"
+const lambdas = 0.0:0.1:0.9
+# const truncations = [1, 10, 24]
+
+
+#------ Optimizers ----------#
+
+# Parameters for the SGD Algorithm
+const optimizer = "Descent"
+const alphas = [0.001,0.01,0.1,0.25,0.5]
+# const alphas = 0.1*1.5.^(-6:1)
+
+# # Parameters for the RMSProp Optimizer
+# const optimizer = "RMSProp"
+# const alphas = [0.0001, 0.001, 0.01]
+
+function make_arguments_rtd(args::Dict{String, String})
     horde = args["horde"]
     alpha = args["alpha"]
     truncation = args["truncation"]
     seed = args["seed"]
-    save_file = "compassworld_gvfn/$(horde)/RTD/Descent_alpha_$(alpha)_truncation_$(truncation)/run_$(seed).jld2"
-    new_args=["--horde", horde, "--truncation", truncation, "--opt", "Descent", "--optparams", alpha, "--seed", seed, "--savefile", save_file]
+    save_file = "compassworld_gvfn/$(horde)/RTD/$(oprimizer)_alpha_$(alpha)_truncation_$(truncation)/run_$(seed).jld2"
+    new_args=["--horde", horde, "--truncation", truncation, "--opt", optimizer, "--optparams", alpha, "--seed", seed, "--savefile", save_file]
     return new_args
 end
 
-
-const alphas = 0.1*1.5.^(-6:1)
+function make_arguments_tdlambda(args::Dict{String, String})
+    horde = args["horde"]
+    alpha = args["alpha"]
+    lambda = args["lambda"]
+    seed = args["seed"]
+    save_file = "compassworld_gvfn/$(horde)/RTD/$(oprimizer)_alpha_$(alpha)_truncation_$(truncation)/run_$(seed).jld2"
+    new_args=["--horde", horde, "--luparams", lambda, "--opt", optimizer, "--optparams", alpha, "--seed", seed, "--savefile", save_file]
+    return new_args
+end
 
 function main()
 
     arg_dict = Dict([
         "horde"=>["rafols", "forward"],
         "alpha"=>alphas,
-        "truncation"=>[1,2,3,4,6,8,10,16,24],
+        "truncation"=>[1, 10, 24],
         "seed"=>collect(1:5)
     ])
 
     arg_list = ["horde", "alpha", "truncation", "seed"]
-    static_args = ["--alg", "RTD", "--steps", "5000000"]
-    args_iterator = ArgIterator(arg_dict, static_args; arg_list=arg_list, make_args=make_arguments)
-    parallel_experiment_args("experiment/compassworld.jl", args_iterator; exp_module_name=:CompassWorldExperiment, exp_func_name=:main_experiment, num_workers=12)
+    static_args = ["--alg", learning_update, "--steps", "5000000"]
+    args_iterator = ArgIterator(arg_dict, static_args; arg_list=arg_list, make_args=learning_update == "RTD" ? make_arguments_rtd : make_arguments_tdlambda)
+    parallel_experiment_args("experiment/compassworld.jl", args_iterator; exp_module_name=:CompassWorldExperiment, exp_func_name=:main_experiment, num_workers=8)
 
 end
 
