e5d87f4381eb44bbeb22a1a42bdad4e577fd0da7
Matt <mkschleg@umail.iu.edu>
2019-03-27 10:26:39 -0600

    Fixed plot script

--- plot/compassworld_plot.jl
+++ plot/compassworld_plot.jl
@@ -5,15 +5,15 @@ using Plots, FileIO, Statistics, ProgressMeter
 
 function plot_gvfn_descent(network, algorithm, alphas, truncation, num_runs; n=1000)
     gr()
-    dir = "../compassworld_gvfn/$(network)/$(algorithm)"
+    dir = "compassworld_gvfn/$(network)/$(algorithm)"
     plt = nothing
     for α in alphas
         for τ in truncation
             runs = Array{Array{Float64, 1}, 1}()
             for r in 1:num_runs
                 file_loc = joinpath(dir, "Descent_alpha_$(α)_truncation_$(τ)/run_$(r).jld2")
-                d = load(loc)
-                push!(runs, reshape(mean(d["out_err_strg"].^2; dims=2), n, Integer(size(d["out_err_strg"])[1]/n)))
+                d = load(file_loc)
+                push!(runs, mean(reshape(mean(d["out_err_strg"].^2; dims=2), n, Integer(size(d["out_err_strg"])[1]/n)); dims=1)[1,:])
             end
             if plt == nothing
                 plt = plot(mean(runs), label="alpha: $(α), truncations: $(τ)")
