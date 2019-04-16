3403fd6449713e7b28ffcb97a69831d31ee47b5f
Matthew Schlegel <mkschleg@gmail.com>
2019-04-16 11:46:10 -0600

    Added tanh experiment

--- parallel/cycleworld_joint_parallel_tanh.jl
+++ parallel/cycleworld_joint_parallel_tanh.jl
@@ -1,10 +1,16 @@
-#!/usr/local/bin/julia
+#!/home/mkschleg/opt/bin/julia
+#SBATCH --array=1-360
+#SBATCH -o joint_out/%A_%a.out # Standard output
+#SBATCH -e joint_out/%A_%a.err # Standard error
+#SBATCH --mem=2000M # Memory request of 2 GB
+#SBATCH --time=01:00:00 # Running time of 1 hours
+#SBATCH --account=def-whitem
 
 using Pkg
+Pkg.activate(".")
+
 using Reproduce
 using Logging
-
-Pkg.activate(".")
 
 const save_loc = "cycleworld_joint_sweep_tanh"
 const exp_file = "experiment/cycleworld_joint.jl"
@@ -14,9 +20,7 @@ const optimizer = "ADAM"
 # const alphas = [0.001; 0.1*1.5.^(-6:2:1); 0.1*1.5.^(1:2:3)]
 const alphas = [0.01]
 const betas = 0.0:0.2:1.0
-const truncations = [1, 2, 4, 6, 8, 10]
-
-# const num_steps = 200000
+const truncations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function make_arguments(args::Dict{String, String})
     horde = args["outhorde"]
@@ -25,7 +29,7 @@ function make_arguments(args::Dict{String, String})
     cell = args["cell"]
     truncation = args["truncation"]
     seed = args["seed"]
-    new_args=["--gvfnhorde", horde, "--truncation", truncation, "--opt", optimizer, "--optparams", alpha, "--cell", cell, "--seed", seed]
+    new_args=["--outhorde", horde, "--truncation", truncation, "--opt", optimizer, "--optparams", alpha, "--beta", beta, "--cell", cell, "--seed", seed]
     return new_args
 end
 
@@ -39,6 +43,9 @@ function main()
         "--numsteps"
         arg_type=Int64
         default=200000
+        "--saveloc"
+        arg_type=String
+        default=save_loc
         "--numjobs"
         action=:store_true
     end
@@ -46,7 +53,8 @@ function main()
     num_workers = parsed["numworkers"]
     
     arg_dict = Dict([
-        "outhorde"=>["onestep", "chain"],
+        # "outhorde"=>["onestep", "chain"],
+        "outhorde"=>["gamma_chain"],
         "alpha"=>alphas,
         "truncation"=>truncations,
         "cell"=>["RNN"],
@@ -59,7 +67,7 @@ function main()
 
     if parsed["numjobs"]
         @info "This experiment has $(length(collect(args_iterator))) jobs."
-        println(collect(args_iterator[num_workers]))
+        println(collect(args_iterator)[num_workers])
         exit(0)
     end
 
