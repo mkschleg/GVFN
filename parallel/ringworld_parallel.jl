f4f9ac75a79ee6fb4c7b5cb374b3ad181912cc48
Matthew Schlegel <mkschleg@cedar1.cedar.computecanada.ca>
2020-02-18 15:11:18 -0800

    minor config changes for RingWorld

--- parallel/ringworld_parallel.jl
+++ parallel/ringworld_parallel.jl
@@ -1,11 +1,11 @@
 #!/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/gcc7.3/julia/1.3.0/bin/julia
 #SBATCH --mail-user=mkschleg@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH -o ring_gvfn_act.out # Standard output
-#SBATCH -e ring_gvfn_act.err # Standard error
+#SBATCH -o ring_gvfn.out # Standard output
+#SBATCH -e ring_gvfn.err # Standard error
 #SBATCH --mem-per-cpu=2000M # Memory request of 2 GB
-#SBATCH --time=00:10:00 # Running time of 12 hours
-#SBATCH --ntasks=8
+#SBATCH --time=12:00:00 # Running time of 12 hours
+#SBATCH --ntasks=128
 #SBATCH --account=rrg-whitem
 
 using Pkg
