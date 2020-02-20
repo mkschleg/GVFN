093ba142ef800490d9cae9989c07ca76fbd53c40
AJ <a.john.jacobsen@gmail.com>
2020-02-20 13:41:40 -0700

    update CC template; update Manifest to Reproduce 0.5.5

--- parallel/timeseries_template.jl
+++ parallel/timeseries_template.jl
@@ -1,9 +1,10 @@
-#!/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/gcc7.3/julia/1.1.0/bin/julia
+#!/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/gcc7.3/julia/1.3.0/bin/julia
 #SBATCH -o TODO.out # Standard output
 #SBATCH -e TODO.err # Standard error
 #SBATCH --mem-per-cpu=2000M # Memory request of 2 GB
 #SBATCH --time=24:00:00 # Running time of 12 hours
 #SBATCH --ntasks=64
+#SBATCH --cpus-per-task=2
 #SBATCH --account=rrg-whitem
 
 using Pkg
@@ -18,19 +19,15 @@ using Reproduce.Config
 # don't use
 @assert false
 
-# === SET THESE ===
+# === SET THIS ===
 const cfg_file = TODO
-const num_workers = TODO
 # =================
 
 const save_loc = joinpath(string(@__DIR__),"..")
 
 nruns = parse!(ConfigManager(cfg_file, save_loc),1)["args"]["nruns"]
 
-create_experiment_dir(save_loc; org_file=true)
+create_experiment_dir(save_loc; org_file=false)
 config_job(cfg_file,
            save_loc,
-           nruns,
-           num_workers=num_workers,
-           extra_args=[save_loc]
-           )
+           nruns)
